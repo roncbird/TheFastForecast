eda6198a20e9eb495d596aa7010f6b9c0843fc77
roncbird <roncbird@gmail.com>
2017-05-09 13:15:02 -0600

    Add code to correct typo in alert dialog from "At Last the Fast", to "The Fast Forcast", per changed the app name.

--- app/src/main/java/com/mycompany/thefastforecast/fragments/WeatherFragment.java
+++ app/src/main/java/com/mycompany/thefastforecast/fragments/WeatherFragment.java
@@ -84,7 +84,6 @@ public class WeatherFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_weather, container, false);
     }
